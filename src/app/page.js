7c304cbdf98d809e0ebfa6e7c0361db7563efd20
Adan Silva <adan.t.silva77@gmail.com>
2025-01-29 16:21:47 -0800

    Address next warnings and use apos; instead

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1><b>Adan Silva's Resume</b></h1>
+      <h1><b>Adan Silva&apos;s Resume</b></h1>
       <h2><b>Contact Information</b></h2>
       <ul>
         <li><b>Email:</b> <Link href="mailto:adan.t.silva77@gmail.com">adan.t.silva77@gmail.com</Link></li>
@@ -14,7 +14,7 @@ export default function Home() {
       <h2><b>My Projects</b></h2>
       <ul>
           <li><b>SwerveBot-2022</b> A template for a swerve drive using CTRE CANcoders, CTRE Pigeon2 gyro, and REV NEO/SparkMaxes. Used during the 2022 offseason and officially adopted by several other teams. Some code was push up to the well known YAGSL swerve library.</li>
-          <li><b>Robot-2023</b>: Source code for a FRC robot designed to play the 2022-2023 FRC season's game: <i>CHARGED UP!</i></li>
+          <li><b>Robot-2023</b>: Source code for a FRC robot designed to play the 2022-2023 FRC season&apos;s game: <i>CHARGED UP!</i></li>
           <li><b>Password Strength Analyzer and Generator:</b> A tool that evaluates the strength of user passwords and suggests stronger alternatives, along with including a password generator that creates secure, randomized passwords based on user-defined criteria.</li>
           <li><b>Ransomware Detection and Prevention Tool</b> A system that monitors file systems for ransomware-like behavior, such as rapid file encryption. It also isolates infected systems and alert administrators before significant damage occurs.</li>
           <li><b>Encrypted File Sharing Platform</b> A secure platform for sharing files with end-to-end encryption. The system includes features like self-destructing files and access control to ensure data security.</li>

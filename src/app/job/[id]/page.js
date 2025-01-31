ab11e42c0169eb3d0c8ff04db198a04ec2ac23d5
Adan Silva <adan.t.silva77@gmail.com>
2025-01-31 12:30:29 -0800

    fix date error

--- src/app/job/[id]/page.js
+++ src/app/job/[id]/page.js
@@ -12,14 +12,17 @@ export default async function Job({ params }) {
     return <div>Job not found</div>;
   }
 
+  let convertedStart = new Date(job.start).toLocaleDateString();
+  let convertedEnd = new Date(job.end).toLocaleDateString();
+
   return (
     <div>
       <h1><b>Additional Job Information</b></h1>
       <br />
       <h3><b>Title:</b> {job.title}</h3>
       <h3><b>Company:</b> {job.company}</h3>
-      <h3><b>Start Date:</b> {job.start}</h3>
-      <h3><b>End Date:</b> {job.end}</h3>
+      <h3><b>Start Date:</b> {convertedStart}</h3>
+      <h3><b>End Date:</b> {convertedEnd}</h3>
       <h3><b>Description:</b> {job.description}</h3>
       <br />
       <h3><Link href="/"><b>Home</b></Link></h3>

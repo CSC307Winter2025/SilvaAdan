f7a08a0cbd39a9acf22d44221ccf330b4fc4c216
Adan Silva <adan.t.silva77@gmail.com>
2025-01-31 17:04:26 -0800

    Implement job editing feature

--- src/actions.js
+++ src/actions.js
@@ -15,3 +15,20 @@ export async function saveJobEntry(data) {
     await prisma.job.create({data: job});
     redirect('/');
 }
+
+export async function updateJobEntry(data) {
+    const jobId = Number(data.get('id'));
+    const updatedJob = {
+        title: data.get('title'),
+        company: data.get('company'),
+        start: new Date(data.get('start')).toISOString(),
+        end: new Date(data.get('end')).toISOString(),
+        description: data.get('description'),
+    }
+
+    await prisma.job.update({
+        where: {id: jobId},
+        data: updatedJob,
+    });
+    redirect(`/job/${jobId}`);
+}
--- src/app/job/[id]/page.js
+++ src/app/job/[id]/page.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import prisma from '@/lib/prisma';
+import { updateJobEntry } from '@/actions';
 
 export default async function Job({ params }) {
   const job = await prisma.job.findUnique({
@@ -26,6 +27,27 @@ export default async function Job({ params }) {
       <h3><b>Description:</b> {job.description}</h3>
       <br />
       <h3><Link href="/"><b>Home</b></Link></h3>
+
+      <h3><b>Edit Job</b></h3>
+      <form action={updateJobEntry}>
+        <input type="hidden" name="id" value={job.id} />
+        <label>Title</label>
+        <input type="text" name="title" defaultValue={job.title} />
+        <br />
+        <label>Company</label>
+        <input type="text" name="company" defaultValue={job.company} />
+        <br />
+        <label>Start Date</label>
+        <input type="date" name="start" defaultValue={job.start.toISOString().split('T')[0]} />
+        <br />
+        <label>End Date</label>
+        <input type="date" name="end" defaultValue={job.end.toISOString().split('T')[0]} />
+        <br />
+        <label>Description</label>
+        <textarea name="description" defaultValue={job.description}></textarea>
+        <br />
+        <button type="submit">Save</button>
+      </form>
     </div>
   );
 }
